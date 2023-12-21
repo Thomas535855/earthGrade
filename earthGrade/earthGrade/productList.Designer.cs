b79f2a6937354246e0dc54cafef3c88a2cbfbb56
Thomas535855 <153903271+Thomas535855@users.noreply.github.com>
2023-12-21 15:10:09 +0100

    Nieuwste versie met geupdate productenlijst

--- earthGrade/earthGrade/productList.Designer.cs
+++ earthGrade/earthGrade/productList.Designer.cs
@@ -30,18 +30,15 @@
         {
             panel1 = new Panel();
             button1 = new Button();
-            infoBox1 = new ListBox();
-            label1 = new Label();
-            infoButton1 = new Button();
-            infoButton2 = new Button();
-            label2 = new Label();
-            infoBox2 = new ListBox();
+            dataGridView1 = new DataGridView();
+            checkBox1 = new CheckBox();
             panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // panel1
             // 
-            panel1.BackColor = SystemColors.ControlDarkDark;
+            panel1.BackColor = Color.FromArgb(64, 64, 64);
             panel1.Controls.Add(button1);
             panel1.Location = new Point(0, 0);
             panel1.Margin = new Padding(3, 4, 3, 4);
@@ -61,79 +58,52 @@
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
-            // infoBox1
+            // dataGridView1
             // 
-            infoBox1.FormattingEnabled = true;
-            infoBox1.ItemHeight = 20;
-            infoBox1.Location = new Point(13, 139);
-            infoBox1.Name = "infoBox1";
-            infoBox1.Size = new Size(129, 144);
-            infoBox1.TabIndex = 2;
-            infoBox1.Visible = false;
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.AllowUserToResizeColumns = false;
+            dataGridView1.AllowUserToResizeRows = false;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.EditMode = DataGridViewEditMode.EditProgrammatically;
+            dataGridView1.Location = new Point(13, 168);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 29;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.Size = new Size(430, 573);
+            dataGridView1.TabIndex = 2;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
             // 
-            // label1
+            // checkBox1
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(12, 108);
-            label1.Name = "label1";
-            label1.Size = new Size(68, 20);
-            label1.TabIndex = 3;
-            label1.Text = "Product1";
-            // 
-            // infoButton1
-            // 
-            infoButton1.Location = new Point(86, 104);
-            infoButton1.Name = "infoButton1";
-            infoButton1.Size = new Size(94, 29);
-            infoButton1.TabIndex = 4;
-            infoButton1.Text = "More info";
-            infoButton1.UseVisualStyleBackColor = true;
-            infoButton1.Click += button2_Click;
-            // 
-            // infoButton2
-            // 
-            infoButton2.Location = new Point(86, 316);
-            infoButton2.Name = "infoButton2";
-            infoButton2.Size = new Size(94, 29);
-            infoButton2.TabIndex = 7;
-            infoButton2.Text = "More info";
-            infoButton2.UseVisualStyleBackColor = true;
-            infoButton2.Click += infoButton2_Click;
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Location = new Point(12, 320);
-            label2.Name = "label2";
-            label2.Size = new Size(68, 20);
-            label2.TabIndex = 6;
-            label2.Text = "Product1";
-            // 
-            // infoBox2
-            // 
-            infoBox2.FormattingEnabled = true;
-            infoBox2.ItemHeight = 20;
-            infoBox2.Location = new Point(13, 351);
-            infoBox2.Name = "infoBox2";
-            infoBox2.Size = new Size(129, 144);
-            infoBox2.TabIndex = 5;
-            infoBox2.Visible = false;
+            checkBox1.AutoSize = true;
+            checkBox1.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point);
+            checkBox1.Location = new Point(13, 138);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(140, 34);
+            checkBox1.TabIndex = 3;
+            checkBox1.Text = "View more";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
             // 
             // productList
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(63, 126, 68);
             ClientSize = new Size(462, 753);
-            Controls.Add(infoButton2);
-            Controls.Add(label2);
-            Controls.Add(infoBox2);
-            Controls.Add(infoButton1);
-            Controls.Add(label1);
-            Controls.Add(infoBox1);
+            Controls.Add(dataGridView1);
             Controls.Add(panel1);
+            Controls.Add(checkBox1);
             Name = "productList";
             Text = "productList";
             panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -142,11 +112,7 @@
 
         private Panel panel1;
         private Button button1;
-        private ListBox infoBox1;
-        private Label label1;
-        private Button infoButton1;
-        private Button infoButton2;
-        private Label label2;
-        private ListBox infoBox2;
+        private DataGridView dataGridView1;
+        private CheckBox checkBox1;
     }
 }
--- earthGrade/earthGrade/productList.cs
+++ earthGrade/earthGrade/productList.cs
@@ -12,22 +12,23 @@ namespace earthGrade
 {
     public partial class productList : Form
     {
+
+
+
         public productList()
         {
             InitializeComponent();
-            product appelNL = new product(1, "appelNL", "1234", 4.5);
-            product appelVS = new product(2, "appelVS", "9876", 2.3);
 
-            label1.Text = appelNL.name;
-            label2.Text = appelVS.name;
+            List<product> productList = new List<product>();
 
-            infoBox1.Items.Add(appelNL.sterren);
-            infoBox1.Items.Add(appelNL.code);
-            infoBox1.Items.Add(appelNL.id);
+            productList.Add(new product(1, "Product 1", "Code1", 2.5));
+            productList.Add(new product(2, "Product 2", "Code1", 4.5));
+            productList.Add(new product(3, "Product 3", "Code1", 1.2));
 
-            infoBox2.Items.Add(appelVS.sterren);
-            infoBox2.Items.Add(appelVS.code);
-            infoBox2.Items.Add(appelVS.id);
+            dataGridView1.DataSource = productList;
+            dataGridView1.Columns["id"].Visible = false;
+            dataGridView1.Columns["code"].Visible = false;
+
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -37,34 +38,26 @@ namespace earthGrade
             form2.Show();
         }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            if (infoBox1.Visible == false)
-            {
-                infoBox1.Visible = true;
-                infoButton1.Text = "Show less";
-            }
-            else
-            {
-                infoBox1.Visible = false;
-                infoButton1.Text = "More info";
-            }
 
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
 
         }
 
-        private void infoButton2_Click(object sender, EventArgs e)
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
-            if (infoBox2.Visible == false)
+            if (checkBox1.Checked)
             {
-                infoBox2.Visible = true;
-                infoButton2.Text = "Show less";
+                dataGridView1.Columns["id"].Visible = true;
+                dataGridView1.Columns["code"].Visible = true;
             }
             else
             {
-                infoBox2.Visible = false;
-                infoButton2.Text = "More info";
+                dataGridView1.Columns["id"].Visible = false;
+                dataGridView1.Columns["code"].Visible = false;
             }
+
         }
     }
 }
